fa1f30c678bc9a71d7314a05839cca38e86291c3
MintuJgit24 <mintujoseph95@gmail.com>
2026-09-10 11:23:01 +0530

    locators

--- tests/locators.spec.js
+++ tests/locators.spec.js
@@ -6,10 +6,26 @@ test.only("Locators", async ({ page }) => {
     await page.goto("https://selenium.qabible.in/form-submit.php")
     //locating using id put a # before the id
     await page.locator("#validationCustom01").fill("Mintu")
-    await page.locator("#validationCustom02").fill("Joseph")
-    await page.locator("#validationCustomUsername").fill("mintu.joseph")
+    //await page.locator("#validationCustom02").fill("Joseph")
+    //await page.locator("#validationCustomUsername").fill("mintu.joseph")
     //to pause the test execution and open the browser for debugging, you can use the page.pause() method. This will halt the test at that point and allow you to inspect the page state, interact with elements, and perform any necessary debugging actions.
     //await page.pause() 
+
+    //for class use . before the class name
+    //index starts from 0, so nth(1) refers to the second element with the class "form-control"
+    await page.locator(".form-control").nth(1).fill("Joseph")
+    //here we give single quotes for value to avoid confusion
+    await page.locator("[placeholder='Username']").fill("mintu.joseph")
+    await page.locator("//input[@placeholder='City']").fill("Kochi")
+    //inbuilt method to locate by placeholder, it is more readable and easier to understand
+    await page.getByPlaceholder("State").fill("Kerala")
+    //label value is visible text Zip
+    await page.getByLabel("Zip").fill("682001")
+    //await page.getByLabel("Agree to terms and conditions").check()
+    //or using click()
+    await page.locator("#invalidCheck").click()
+    await page.getByRole("button", { name: "Submit form" }).click()
     await page.waitForTimeout(3000)//3sec it will wait and then got closed
+
 })
 
