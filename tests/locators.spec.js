9e348f75f0c341be0514f8bc16618cd433d29a12
MintuJgit24 <mintujoseph95@gmail.com>
2026-09-10 12:09:59 +0530

    task

--- tests/locators.spec.js
+++ tests/locators.spec.js
@@ -2,7 +2,7 @@ import { test } from "@playwright/test"
 //page fixture is used to create a new page within the browser context, allowing for interaction with web pages
 // and performing actions like navigation, clicking, and form submission.
 //.only is used to run only the specified test and skip all other tests in the file. This is useful for debugging or focusing on a specific test case.
-test.only("Locators", async ({ page }) => {
+test("Locators", async ({ page }) => {
     await page.goto("https://selenium.qabible.in/form-submit.php")
     //locating using id put a # before the id
     await page.locator("#validationCustom01").fill("Mintu")
@@ -21,9 +21,9 @@ test.only("Locators", async ({ page }) => {
     await page.getByPlaceholder("State").fill("Kerala")
     //label value is visible text Zip
     await page.getByLabel("Zip").fill("682001")
-    //await page.getByLabel("Agree to terms and conditions").check()
+    await page.getByLabel("Agree to terms and conditions").check()
     //or using click()
-    await page.locator("#invalidCheck").click()
+    //await page.locator("#invalidCheck").click()
     await page.getByRole("button", { name: "Submit form" }).click()
     await page.waitForTimeout(3000)//3sec it will wait and then got closed
 
--- tests/sample.spec.js
+++ tests/sample.spec.js
@@ -1,5 +1,11 @@
-import {test} from "@playwright/test"
+import { test } from "@playwright/test"
 
-test("sample",async({page})=>{
-
+test.only("sample", async ({ page }) => {
+    await page.goto("https://selenium.qabible.in/simple-form-demo.php")
+    await page.locator("#single-input-field").fill("Hello Mintu")
+    await page.locator("#button-one").click()
+    await page.locator("#value-a").fill("10")
+    await page.locator("#value-b").fill("20")
+    await page.getByRole("button", { name: "Get Total" }).click()
+    await page.waitForTimeout(3000)
 })
